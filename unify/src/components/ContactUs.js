b0dc0e9a96acb3df239aff4c70c30d538b5c1e5d
Aditya jha <medoremon0001@gmail.com>
2025-02-23 10:33:09 +0530

    Create ContactUs.js

---
+++ unify/src/components/ContactUs.js
@@ -0,0 +1,13 @@
+import React from "react";
+import './styles/ContactUs.css';
+
+const ContactUs = () => {
+    return (
+        <section>
+            <h2>Contact Us</h2>
+            <p>Email: support@unify.com</p>
+        </section>
+    );
+};
+
+export default ContactUs;
