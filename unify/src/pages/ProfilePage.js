a90937cf89d9d5edc620eca6472e5b7337c806bf
Aditya jha <medoremon0001@gmail.com>
2025-02-23 10:34:31 +0530

    Create ProfilePage.js

---
+++ unify/src/pages/ProfilePage.js
@@ -0,0 +1,13 @@
+import React from "react";
+import './styles/ProfilePage.css';
+
+const ProfilePage = () => {
+    return (
+        <div>
+            <h2>Welcome to Your Profile</h2>
+            <p>This is where your profile details will be displayed.</p>
+        </div>
+    );
+};
+
+export default ProfilePage;
