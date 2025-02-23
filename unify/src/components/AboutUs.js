f5cce1e60621b62d02cb33a7efcba3bf03fce29b
Aditya jha <medoremon0001@gmail.com>
2025-02-23 10:33:04 +0530

    Create AboutUs.js

---
+++ unify/src/components/AboutUs.js
@@ -0,0 +1,12 @@
+import React from "react";
+import './styles/AboutUs.css';
+const AboutUs = () => {
+    return (
+        <section>
+            <h2>About Us</h2>
+            <p>UniFY is a platform designed to foster interaction, knowledge sharing, and community building for university students.</p>
+        </section>
+    );
+};
+
+export default AboutUs;
